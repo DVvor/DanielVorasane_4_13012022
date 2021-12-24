d0dfa428fb2d97f580a819a3974803b488435f54
DVvor <85024600+DVvor@users.noreply.github.com>
2021-12-24 14:22:54 +0100

    close modal function

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,18 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
+const close = document.querySelector(".close");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+close.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
